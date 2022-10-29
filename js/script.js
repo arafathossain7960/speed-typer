52c9ba29a03a4b10a153ce094ceae5eaa6679b0e
Arafat Hossaine <arafathossain7960@gmail.comm>
2022-10-29 15:34:40 +0600

    6th problem has been solved.mistake counting

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ fetch("./texts.json")
 // checks the user typed character and displays accordingly
 const typeController = (e) => {
   const newLetter = e.key;
-  console.log(newLetter)
+  
 
   // Handle backspace press
   if (newLetter == "Backspace") {
@@ -46,6 +46,7 @@ const typeController = (e) => {
   if (newLetterCorrect) {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
+    errorCount++;
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
   }
 
